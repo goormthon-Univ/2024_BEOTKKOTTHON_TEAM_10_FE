72ba1e602d4379bfcee4a16ec1be7bec123c540b
jeoungsung12 <jeoungsung12@naver.com>
2024-03-20 03:36:51 +0900

    [Feat] Set UNUserNotificaitonCenterDelegate

--- CenApp/App/Delegate/SceneDelegate.swift
+++ CenApp/App/Delegate/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else {return}
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let viewController = HomeViewController()
+        let viewController = LoginViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
