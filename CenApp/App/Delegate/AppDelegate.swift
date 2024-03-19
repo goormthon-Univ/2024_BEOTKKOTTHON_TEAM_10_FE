0a8beffd3a70302f6a876989bdb5d45197d29e5d
jeoungsung12 <jeoungsung12@naver.com>
2024-03-20 03:34:05 +0900

    [Feat] Set UNUserNotificaitonCenterDelegate

--- CenApp/App/Delegate/AppDelegate.swift
+++ CenApp/App/Delegate/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        self.registerForRemoteNotifications()
         let viewController = HomeViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window?.rootViewController = navigationController
@@ -27,8 +28,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
-
-
 }
-
+//MARK: - Server Notification
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    private func registerForRemoteNotifications() {
+        // 1. 푸시 center (유저에게 권한 요청 용도)
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        // push처리에 대한 delegate - UNUserNotificationCenterDelegate
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        center.requestAuthorization(options: options) { (granted, error) in
+            guard granted else {
+                return
+            }
+            DispatchQueue.main.async {
+                // 2. APNs에 디바이스 토큰 등록
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    // 3. 앱이 foreground상태 일 때, 알림이 온 경우 어떻게 표현할 것인지 처리
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // 푸시가 오면 alert, badge, sound표시를 하라는 의미
+        completionHandler([.alert, .badge, .sound])
+    }
+    // push를 탭한 경우 처리
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // deep link처리 시 아래 url값 가지고 처리
+        let url = response.notification.request.content.userInfo
+        print("url = \(url)")
+    }
+    //디바이스 토큰 등록 성공 시 실행 메서드
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let deviceTokenString = deviceToken.map { String(format: "%02x", $0) }.joined()
+        print("디바이스 토큰\(deviceTokenString)")
+    }
+    //디바이스 토큰 등록 실패 시 실행 메서드
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+    }
+}
 
--- CenApp/App/Delegate/SceneDelegate.swift
+++ CenApp/App/Delegate/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else {return}
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let viewController = LoginViewController()
+        let viewController = HomeViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
