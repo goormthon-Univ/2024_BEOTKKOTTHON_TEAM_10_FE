621ef0cbe77437275d0a9182a6962b6b6a903722
minsolkim <123alsthf@naver.com>
2024-03-19 21:02:07 +0900

    merge F1

---
+++ CenApp/App/Delegate/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  CenApp
+//
+//  Created by 김민솔 on 3/16/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+
+
+}
+
+
---
+++ CenApp/Core/Color + Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Color + Extension.swift
+//  CenApp
+//
+//  Created by 김민솔 on 3/16/24.
+//
+
+import Foundation
+import UIKit
+extension UIColor {
+    static var PrimaryColor : UIColor {
+        return UIColor(named: "PrimaryColor") ?? .white
+    }
+    static var SecondaryColor : UIColor {
+        return UIColor(named: "SecondaryColor") ?? .white
+    }
+    static var ThirdaryColor : UIColor {
+        return UIColor(named: "ThirdaryColor") ?? .white
+    }
+}
+
