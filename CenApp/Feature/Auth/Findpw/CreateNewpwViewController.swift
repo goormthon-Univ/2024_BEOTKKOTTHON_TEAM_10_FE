fa8a37a349a2af3a153c00f6dcb151c655e44bb7
jeoungsung12 <jeoungsung12@naver.com>
2024-03-21 18:41:18 +0900

    [Feat] CreateNewpwVC

--- CenApp/Feature/Auth/Findpw/CreateNewpwViewController.swift
+++ CenApp/Feature/Auth/Findpw/CreateNewpwViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FindpwViewController.swift
+//  CreateNewpwViewController.swift
 //  CenApp
 //
 //  Created by 정성윤 on 2024/03/21.
@@ -11,8 +11,16 @@ import Alamofire
 import Then
 import Kingfisher
 import UIKit
-class FindpwViewController : UIViewController, UITextFieldDelegate{
+class CreateNewpwViewController : UIViewController, UITextFieldDelegate{
     // MARK: - UI Components
+    var post : [String]
+    init(post: [String]) {
+        self.post = post
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     //탭 제스처
     private lazy var tapGesture : UITapGestureRecognizer = {
         let gesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
@@ -21,70 +29,10 @@ class FindpwViewController : UIViewController, UITextFieldDelegate{
     //타이틀
     private let titleLabel : UILabel = {
         let label = UILabel()
-        label.text = "비밀번호 찾기"
+        label.text = "새비밀번호 등록"
         label.textColor = .black
         label.textAlignment = .center
         label.font = UIFont.systemFont(ofSize: 20)
-        return label
-    }()
-    //이름
-    private let nameLabel : UILabel = {
-        let label = UILabel()
-        label.text = "이름"
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 15)
-        label.backgroundColor = .white
-        return label
-    }()
-    private let nameView : UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 10
-        view.layer.masksToBounds = true
-        view.layer.borderColor = UIColor.lightGray.cgColor
-        view.layer.borderWidth = 0.3
-        return view
-    }()
-    private let nameText : UITextField = {
-        let text = UITextField()
-        text.backgroundColor = .white
-        text.placeholder = "Name"
-        text.textColor = .black
-        text.textAlignment = .left
-        return text
-    }()
-    //아이디
-    private let idLabel : UILabel = {
-        let label = UILabel()
-        label.text = "아이디"
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 15)
-        label.backgroundColor = .white
-        return label
-    }()
-    private let idView : UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 10
-        view.layer.masksToBounds = true
-        view.layer.borderColor = UIColor.lightGray.cgColor
-        view.layer.borderWidth = 0.3
-        return view
-    }()
-    private let idText : UITextField = {
-        let text = UITextField()
-        text.backgroundColor = .white
-        text.placeholder = "ID"
-        text.textColor = .black
-        text.textAlignment = .left
-        return text
-    }()
-    private let idDescription : UILabel = {
-        let label = UILabel()
-        label.text = ""
-        label.textColor = .red
-        label.font = UIFont.systemFont(ofSize: 11)
-        label.backgroundColor = .white
         return label
     }()
     //비밀번호
@@ -130,11 +78,54 @@ class FindpwViewController : UIViewController, UITextFieldDelegate{
         label.backgroundColor = .white
         return label
     }()
+    //비밀번호
+    private let repwLabel : UILabel = {
+        let label = UILabel()
+        label.text = "비밀번호"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.backgroundColor = .white
+        return label
+    }()
+    private let repwView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        view.layer.borderWidth = 0.3
+        return view
+    }()
+    private let repwText : UITextField = {
+        let text = UITextField()
+        text.backgroundColor = .white
+        text.placeholder = "Password"
+        text.textColor = .black
+        text.textAlignment = .left
+        text.isSecureTextEntry = true
+        return text
+    }()
+    private lazy var repwSecureBtn : UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .white
+        btn.setImage(UIImage(systemName: "eye"), for: .normal)
+        btn.tintColor = .black
+        btn.addTarget(self, action: #selector(resecureBtnTapped), for: .touchUpInside)
+        return btn
+    }()
+    private let repwDescription : UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.textColor = .red
+        label.font = UIFont.systemFont(ofSize: 11)
+        label.backgroundColor = .white
+        return label
+    }()
     //로그인 버튼
     private lazy var loginBtn : UIButton = {
         let btn = UIButton()
         btn.backgroundColor = .cGray
-        btn.setTitle("확인", for: .normal)
+        btn.setTitle("생성", for: .normal)
         btn.setTitleColor(.gray, for: .normal)
         btn.layer.cornerRadius = 15
         btn.layer.masksToBounds = true
@@ -153,26 +144,29 @@ class FindpwViewController : UIViewController, UITextFieldDelegate{
     }
 }
 // MARK: - UI Layout
-extension FindpwViewController {
+extension CreateNewpwViewController {
     private func setLayout() {
         self.view.backgroundColor = .white
         self.view.addGestureRecognizer(tapGesture)
         self.view.clipsToBounds = true
-        self.title = ""
-        self.nameText.delegate = self
-        self.idText.delegate = self
+        self.pwText.delegate = self
+        self.repwText.delegate = self
         //타이틀
         self.view.addSubview(titleLabel)
-        //이름
-        self.view.addSubview(nameLabel)
-        self.nameView.addSubview(nameText)
-        self.view.addSubview(nameView)
         
-        //아이디
-        self.view.addSubview(idLabel)
-        self.idView.addSubview(idText)
-        self.view.addSubview(idView)
-        self.view.addSubview(idDescription)
+        //비밀번호
+        self.view.addSubview(pwLabel)
+        self.pwView.addSubview(pwText)
+        self.pwView.addSubview(pwSecureBtn)
+        self.view.addSubview(pwView)
+        self.view.addSubview(pwDescription)
+        
+        //비밀번호 재확인
+        self.view.addSubview(repwLabel)
+        self.repwView.addSubview(repwText)
+        self.repwView.addSubview(repwSecureBtn)
+        self.view.addSubview(repwView)
+        self.view.addSubview(repwDescription)
         
         //로그인
         self.view.addSubview(loginBtn)
@@ -184,41 +178,52 @@ extension FindpwViewController {
             make.leading.trailing.equalToSuperview().inset(0)
             make.top.equalToSuperview().offset(self.view.frame.height/8)
         }
-        //이름
-        nameLabel.snp.makeConstraints { make in
+        //비밀번호
+        pwLabel.snp.makeConstraints { make in
             make.left.trailing.equalToSuperview().inset(30)
             make.center.equalToSuperview().offset(-100)
         }
-        nameText.snp.makeConstraints { make in
-            make.leading.trailing.top.bottom.equalToSuperview().inset(10)
+        pwText.snp.makeConstraints { make in
+            make.leading.top.bottom.equalToSuperview().inset(10)
+            make.trailing.equalToSuperview().inset(50)
         }
-        nameView.snp.makeConstraints { make in
+        pwSecureBtn.snp.makeConstraints { make in
+            make.trailing.top.bottom.equalToSuperview().inset(10)
+        }
+        pwView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(30)
-            make.top.equalTo(nameLabel.snp.bottom).offset(10)
+            make.top.equalTo(pwLabel.snp.bottom).offset(10)
             make.height.equalTo(40)
+        }
+        pwDescription.snp.makeConstraints { make in
+            make.top.equalTo(pwView.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(30)
         }
         
-        //아이디
-        idLabel.snp.makeConstraints { make in
+        repwLabel.snp.makeConstraints { make in
             make.left.trailing.equalToSuperview().inset(30)
-            make.top.equalTo(nameView.snp.bottom).offset(30)
+            make.top.equalTo(pwDescription.snp.bottom).offset(10)
         }
-        idText.snp.makeConstraints { make in
-            make.leading.trailing.top.bottom.equalToSuperview().inset(10)
+        repwText.snp.makeConstraints { make in
+            make.leading.top.bottom.equalToSuperview().inset(10)
+            make.trailing.equalToSuperview().inset(50)
         }
-        idView.snp.makeConstraints { make in
+        repwSecureBtn.snp.makeConstraints { make in
+            make.trailing.top.bottom.equalToSuperview().inset(10)
+        }
+        repwView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(30)
-            make.top.equalTo(idLabel.snp.bottom).offset(10)
+            make.top.equalTo(repwLabel.snp.bottom).offset(10)
             make.height.equalTo(40)
         }
-        idDescription.snp.makeConstraints { make in
-            make.top.equalTo(idView.snp.bottom).offset(10)
+        repwDescription.snp.makeConstraints { make in
+            make.top.equalTo(repwView.snp.bottom).offset(10)
             make.leading.trailing.equalToSuperview().inset(30)
         }
         
         //로그인
         loginBtn.snp.makeConstraints { make in
-            make.top.equalTo(idDescription.snp.bottom).offset(100)
+            make.top.equalTo(repwDescription.snp.bottom).offset(100)
             make.leading.trailing.equalToSuperview().inset(30)
             make.height.equalTo(50)
         }
@@ -231,10 +236,10 @@ extension FindpwViewController {
     }
 }
 // MARK: - TextDelegate
-extension FindpwViewController {
+extension CreateNewpwViewController {
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        if textField == idText || textField == nameText{
-            if !(idText.text?.isEmpty ?? true) && !(nameText.text?.isEmpty ?? true)  {
+        if textField == pwText || textField == repwText{
+            if !(pwText.text?.isEmpty ?? true) && !(repwText.text?.isEmpty ?? true)  {
                 loginBtn.backgroundColor = .PrimaryColor
                 loginBtn.setTitleColor(.white, for: .normal)
                 loginBtn.isEnabled = true
@@ -245,22 +250,31 @@ extension FindpwViewController {
                 loginBtn.isEnabled = false
             }
         }
+        if textField == repwText {
+            if pwText.text == repwText.text {
+                repwDescription.text = "* 비밀번호가 일치합니다"
+                repwDescription.textColor = .black
+            }else {
+                repwDescription.text = "* 비밀번호가 일치하지 않습니다"
+                repwDescription.textColor = .red
+            }
+        }
     }
 }
 
 // MARK: - Actions
-extension FindpwViewController {
+extension CreateNewpwViewController {
     @objc private func hideKeyboard() {
         view.endEditing(true)
     }
     @objc private func loginBtnTapped() {
         self.loadingIndicator.startAnimating()
         self.pwDescription.text = ""
-        self.idDescription.text = ""
-        if  let name = nameText.text,
-            let id = idText.text{
+        self.repwDescription.text = ""
+        if  let pw = pwText.text,
+            let repw = repwText.text{
             //수정필요
-            self.navigationController?.pushViewController(CreateNewpwViewController(post: [name, id]), animated: true)
+            
 //            SignupService.requestSignup(userInfo: SignupModel(userid: id, password: "", name: name)) { result in
 //                if let result = result {
 //                    if result.message == "success" {
@@ -285,5 +299,8 @@ extension FindpwViewController {
     }
     @objc private func secureBtnTapped() {
         self.pwText.isSecureTextEntry.toggle()
+    }
+    @objc private func resecureBtnTapped() {
+        self.repwText.isSecureTextEntry.toggle()
     }
 }
